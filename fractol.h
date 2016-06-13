15920762c70b1908add28dbb9b0986c49d926b36
Alexis FILLION <afillion@e3r1p4.42.fr>
2016-06-13 17:01:25 +0200

    arret de pixel put, utilisation d'image comme recommande, ajout fcnt pixel put to img et creation d une struct mandelbrot aka t_mand

--- fractol.h
+++ fractol.h
@@ -7,6 +7,34 @@ typedef struct		s_env
 {
 	void			*mlx;
 	void			*win;
+	void			*img;
+	char			*data;
+	int				bpp;
+	int				size_line;
+	int				endian;
+	int				color;
 }					t_env;
+
+typedef	struct		s_mandelbrot
+{
+	int				x;
+	int				y;
+	double			x1;
+	double			x2;
+	double			y1;
+	double			y2;
+	double			zoom;
+	double			max;
+	double			image_x;
+	double			image_y;
+	double			c_r;
+	double			c_i;
+	double			z_r;
+	double			z_i;
+	double			i;
+	double			tmp;
+}					t_mand;
+
+void				put_pixel_to_img(t_env *e, int x, int y);
 
 #endif
--- main.c
+++ main.c
@@ -2,22 +2,22 @@
 
 void	draw(t_env e)
 {
-	int x;
-	int y;
-	double x1;
-	double x2;
-	double y1;
-	double y2;
-	double zoom;
-	double max;
-	double image_x;
-	double image_y;
-	double c_r;
-	double c_i;
-	double z_r;
-	double z_i;
-	double i;
-	double tmp;
+	int		x;
+	int		y;
+	double	x1;
+	double	x2;
+	double	y1;
+	double	y2;
+	double	zoom;
+	double	max;
+	double	image_x;
+	double	image_y;
+	double	c_r;
+	double	c_i;
+	double	z_r;
+	double	z_i;
+	double	i;
+	double	tmp;
 
 	i = 0;
 	x = 0;
@@ -30,6 +30,8 @@ void	draw(t_env e)
 	max = 50;
 	image_x = (x2 - x1) * zoom;
 	image_y = (y2 - y1) * zoom;
+	e.img = mlx_new_image(e.mlx, (int)image_x, (int)image_y);
+	e.data = mlx_get_data_addr(e.img, &e.bpp, &e.size_line, &e.endian);
 	while (x < image_x)
 	{
 		y = 0;
@@ -49,14 +51,29 @@ void	draw(t_env e)
 			}
 			if (i == max)
 			{
-				mlx_pixel_put(e.mlx, e.win, x, y, 0x000000);
+				e.color = 0x000000;
+				put_pixel_to_img(&e, x, y);
 			}
 			else
-				mlx_pixel_put(e.mlx, e.win, x, y, (i*255) / max);
+			{
+				e.color = (i * 255) / max;
+				put_pixel_to_img(&e, x, y);
+			}
 			y++;
 		}
 		x++;
 	}
+	mlx_put_image_to_window(e.mlx, e.win, e.img, 0, 0);
+}
+
+void	put_pixel_to_img(t_env *e, int x, int y)
+{
+	int	pos;
+
+	pos = (x * e->bpp / 8) + (y * e->size_line);
+	e->data[pos] = e->color % 256;
+	e->data[pos + 1] = (e->color >> 8) % 256;
+	e->data[pos + 2] = (e->color >> 16) % 256;
 }
 
 int		expose_hook(t_env *e)
