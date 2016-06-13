37a223f17da33a827fbb47c319bcb0ab28b9f4ad
Alexis FILLION <afillion@e3r1p4.42.fr>
2016-06-13 17:24:59 +0200

    ..

--- main.c
+++ main.c
@@ -2,66 +2,51 @@
 
 void	draw(t_env e)
 {
-	int		x;
-	int		y;
-	double	x1;
-	double	x2;
-	double	y1;
-	double	y2;
-	double	zoom;
-	double	max;
-	double	image_x;
-	double	image_y;
-	double	c_r;
-	double	c_i;
-	double	z_r;
-	double	z_i;
-	double	i;
-	double	tmp;
+	t_mand	m;
 
-	i = 0;
-	x = 0;
-	y = 0;
-	x1 = -2.1;
-	x2 = 0.6;
-	y1 = -1.2;
-	y2 = 1.2;
-	zoom = 100;
-	max = 50;
-	image_x = (x2 - x1) * zoom;
-	image_y = (y2 - y1) * zoom;
-	e.img = mlx_new_image(e.mlx, (int)image_x, (int)image_y);
+	m.i = 0;
+	m.x = 0;
+	m.y = 0;
+	m.x1 = -2.1;
+	m.x2 = 0.6;
+	m.y1 = -1.2;
+	m.y2 = 1.2;
+	m.zoom = 100;
+	m.max = 50;
+	m.image_x = (m.x2 - m.x1) * m.zoom;
+	m.image_y = (m.y2 - m.y1) * m.zoom;
+	e.img = mlx_new_image(e.mlx, (int)m.image_x, (int)m.image_y);
 	e.data = mlx_get_data_addr(e.img, &e.bpp, &e.size_line, &e.endian);
-	while (x < image_x)
+	while (m.x < m.image_x)
 	{
-		y = 0;
-		while (y < image_y)
+		m.y = 0;
+		while (m.y < m.image_y)
 		{
-			c_r = x / zoom + x1;
-			c_i = y / zoom + y1;
-			z_r = 0;
-			z_i = 0;
-			i = 0;
-			while (z_r * z_r + z_i * z_i < 4 && i < max)
+			m.c_r = m.x / m.zoom + m.x1;
+			m.c_i = m.y / m.zoom + m.y1;
+			m.z_r = 0;
+			m.z_i = 0;
+			m.i = 0;
+			while (m.z_r * m.z_r + m.z_i * m.z_i < 4 && m.i < m.max)
 			{
-				tmp = z_r;
-				z_r = z_r * z_r - z_i * z_i + c_r;
-				z_i = 2 * z_i * tmp + c_i;
-				i++;
+				m.tmp = m.z_r;
+				m.z_r = m.z_r * m.z_r - m.z_i * m.z_i + m.c_r;
+				m.z_i = 2 * m.z_i * m.tmp + m.c_i;
+				m.i++;
 			}
-			if (i == max)
+			if (m.i == m.max)
 			{
 				e.color = 0x000000;
-				put_pixel_to_img(&e, x, y);
+				put_pixel_to_img(&e, m.x, m.y);
 			}
 			else
 			{
-				e.color = (i * 255) / max;
-				put_pixel_to_img(&e, x, y);
+				e.color = (m.i * 255) / m.max;
+				put_pixel_to_img(&e, m.x, m.y);
 			}
-			y++;
+			m.y++;
 		}
-		x++;
+		m.x++;
 	}
 	mlx_put_image_to_window(e.mlx, e.win, e.img, 0, 0);
 }
@@ -95,7 +80,7 @@ int		main(void)
 	t_env	e;
 
 	e.mlx = mlx_init();
-	e.win = mlx_new_window(e.mlx, 240, 240, "fractol");
+	e.win = mlx_new_window(e.mlx, 500, 500, "fractol");
 	mlx_expose_hook(e.win, expose_hook, &e);
 	mlx_key_hook(e.win, key_hook, &e);
 	mlx_loop(e.mlx);
